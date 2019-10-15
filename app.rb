a221e13212d2277c5227dfb7abaf7e4cbac02b7e
itggot-david-jensen <david.jensen@elev.ga.ntig.se>
2019-10-15 13:24:19 +0200

    Moved the clock position to upper-right corner from upper-left corner

--- app.rb
+++ app.rb
@@ -16,17 +16,30 @@ class MirrorWindow < Gosu::Window
     super(@window_width, @window_height)
     @font = Gosu::Font.new(self, Gosu.default_font_name, 20)
     Translation.new(language, @server_url)
+
+    @margin = 20
+
+    @scale_x = 1.4
+    @scale_y = 1.4
+    
+    @width_offsets = []
+    @width_offsets << (@font.text_width("<b>#{TimeComponent.new.time}</b>")*@scale_x)
+    @width_offsets << (@font.text_width("#{TimeComponent.new.date.reverse[0]}"\
+      " #{TimeComponent.new.date.reverse[1]} #{TimeComponent.new.date.reverse[2]}")*@scale_x)
+    p @width_offsets
+
   end
-
+  
   def update; end
-
+  
   def draw
-    @font.draw_markup("<b>#{TimeComponent.new.time}</b>", 20, 20, 1, scale_x = 1.4, scale_y = 1.4)
+    @font.draw_markup("<b>#{TimeComponent.new.time}</b>", (@window_width - @width_offsets[0] - @margin), @margin, 1, scale_x = @scale_x, scale_y = @scale_y)
     @font.draw_markup("#{TimeComponent.new.date.reverse[0]}"\
       " #{TimeComponent.new.date.reverse[1]} #{TimeComponent.new.date.reverse[2]}",
-                      20, 60, 1, scale_x = 1.4, scale_y = 1.4)
+      (@window_width - @width_offsets[1] - @margin), 60, 1, scale_x = @scale_x, scale_y = @scale_y)
+    end
   end
-end
-
-window = MirrorWindow.new('en')
-window.show
+  
+  window = MirrorWindow.new('en')
+  window.show
+  
