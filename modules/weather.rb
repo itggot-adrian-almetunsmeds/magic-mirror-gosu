e6c3422e93ddf33608ea372a398619f8155ae227
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2019-11-05 20:47:20 +0100

    :hammer: Implemented cookie retrival methods, to be used when connecting to ws.

--- modules/weather.rb
+++ modules/weather.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Faye::WebSocket.new
 
 # ws.on(:open/:close/:message)
@@ -9,33 +11,51 @@
 require 'faye'
 require 'faye/websocket'
 require 'eventmachine'
+require 'mechanize'
 
-ws = Faye::WebSocket::Client.new('ws://localhost:9292/')
+# Tries to establish a ws connection
+#
+# cookie - String containing 'rack.session=xyz'
+def ws_establish(cookie)
+  EM.run do
+    hedaders = { headers: { 'Cookie' => cookie } }
+    p hedaders
+    p '_______________-'
+    # Refer to this https://www.rubydoc.info/github/faye/faye-websocket-ruby/file/README.md
+    # for setting up this
+    # TODO: FIXME: THIS IS NOT DONE YET.
+    ws = Faye::WebSocket::Client.new('ws://localhost:9292/', nil, hedaders)
 
-p '-----------------------------'
-p 'Websocket'
-p ws
-p '-----------------------------'
-
-if ws
-    ws.on(:open) do |event|
-
-        p '-----------------------------'
-        p"connection open"
-        p '-----------------------------'
-
+    ws.on :open do |_event|
+      p '-----------------------------'
+      p 'connection open'
+      p '-----------------------------'
     end
 
-    ws.on(:close) do |event|
-        
-        p '-----------------------------'
-        p "connection closed"
-        p '-----------------------------'
-
+    ws.on :message do |msg|
+      p msg
     end
+
+    ws.on :close do |_event|
+      p '-----------------------------'
+      p 'connection closed'
+      p '-----------------------------'
+    end
+  end
 end
-# EM.run {
 
+# Uses Mechanize gem to simulate a browser
+a = Mechanize.new do |agent|
+  agent.user_agent_alias = 'Mac Safari'
+end
 
-
-# }
+# Connects to the server and signs in using the given credentials
+# Then retrives the rack cookie for authentication in ws handshake
+a.get('http://localhost:9292/login') do |page|
+  res = page.form_with(action: '/login') do |login|
+    login.name = 'admin'
+    login.password = 'admin'
+  end.submit
+  cookie = page.header['set-cookie']
+  ws_establish(cookie.split(';').first)
+end
