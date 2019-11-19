8d696c97107ab14f54ce8a00f3e518b65222d204
TODO <todo-actions[bot]@users.noreply.github.com>
2019-11-19 08:30:16 +0000

    Update TODO references: #4, #5, #6

--- modules/translation.rb
+++ modules/translation.rb
@@ -5,7 +5,7 @@ require_relative 'web_handler.rb'
 
 class Translation
   def initialize(language, server_url)
-    # TODO [$5dd08c3f9396ae000c795554]: Make it use the avaliable translation when it is
+    # TODO [#5]: Make it use the avaliable translation when it is
     # unable to fetch one from the back end
     response = WebHandler.request("http://#{server_url}/api/"\
         "translations/#{language}")
